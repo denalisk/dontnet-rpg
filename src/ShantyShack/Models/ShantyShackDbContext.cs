088cff477bb1babfea4ac02d5c106f18d6e03dae
Chad Durkin and Sam Kirsch <chad+sam@>
2017-04-26 09:18:57 -0700

    create base directories and files'

---
+++ src/ShantyShack/Models/ShantyShackDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShantyShack.Models
+{
+    public class ShantyShackDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ShantyShackDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
